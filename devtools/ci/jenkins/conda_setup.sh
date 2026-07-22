dc14e7519238f17b3c05d170c0f13dfaa1d1c477
Evan Pretti <pretti@stanford.edu>
2026-07-22 14:29:59 -0700

    Debugging

---
+++ devtools/ci/jenkins/conda_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -ex
+
+miniforge="Miniforge3-$(uname)-$(uname -m).sh"
+
+wget -q https://github.com/conda-forge/miniforge/releases/latest/download/$miniforge
+chmod +x $miniforge
+./$miniforge -b -c
+conda install -q -y cmake make cython swig doxygen numpy setuptools
