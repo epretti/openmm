1bd51623611bb725e9f042572aab0475ceb10877
Evan Pretti <pretti@stanford.edu>
2026-07-22 16:35:47 -0700

    Debugging

--- devtools/ci/jenkins/install_and_test_cpu.sh
+++ devtools/ci/jenkins/install_and_test_cpu.sh
@@ -2,9 +2,9 @@
 
 EXTRA_CMAKE_ARGS="-DOPENMM_BUILD_CUDA_LIB=false -DOPENMM_BUILD_OPENCL_LIB=false"
 . devtools/ci/jenkins/install.sh
-python devtools/run-ctest.py --job-duration=120 --timeout 300 --in-order -R 'Test(Cpu|Reference)' --parallel 4
+python devtools/run-ctest.py --job-duration=120 --timeout 300 -R 'Test(Cpu|Reference)' --parallel 6
 
 # Build & test Python
 make PythonInstall
 python -m openmm.testInstallation
-cd python/tests && py.test -v
+cd python/tests && pytest -n 6 -v
